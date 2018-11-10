0eef583874e9669e1fa7ba293c886aa051775ea9
BoooooooooooDy.! <42409047+Booooody@users.noreply.github.com>
2018-11-10 10:29:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -741,7 +741,7 @@ client.on('guildMemberAdd', member => {
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "اسم روم الولكم");
+    const logChannel = member.guild.channels.find(channel => channel.name === "words");
     logChannel.send(`Invited by: <@${inviter.id}>`);
   });
 });
