8f0b92c4ed049a57822a70fb9159e8f4918e0c71
BoooooooooooDy.! <42409047+Booooody@users.noreply.github.com>
2018-11-24 09:21:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -803,7 +803,7 @@ client.on('guildMemberAdd', msg => {
     .addField(' 👤   You Number',`**[ ${msg.guild.memberCount} ]**`,true)
     .setColor('GREEN')
     .setFooter(msg.guild.name, msg.guild.iconURL, true)
-    var channel = msg.guild.channels.find('name', 'chat')         
+    var channel = msg.guild.channels.find('name', 'words')         
     if (!channel) return;
     channel.send({embed : embed});
     });
